d1a6e77e0010b91bfbae6fbb034b8c4ce655fe49
Joigca <joseigualda@gmail.com>
2015-10-29 09:46:20 +0100

    Initial commit

---
+++ app/src/main/java/dam2/actividad_01_di/MainActivity.java
@@ -0,0 +1,15 @@
+package dam2.actividad_01_di;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+        //Jose Igualada Calero
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
